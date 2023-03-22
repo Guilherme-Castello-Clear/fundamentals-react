0acc86f37fedd4d42d4d05ddc47c08c94f50c95b
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-03-22 09:04:54 -0300

    Feat: Added my first component

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Primeiro from './components/basicos/Primeiro.js';
+
+
+ReactDOM.render(
+    <div>
+        <Primeiro></Primeiro>
+    </div>,
+    document.getElementById('root')
+);
