3c4426d410018e5998dd7d411e011314bb5e7d8a
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-03-22 09:39:06 -0300

    Feat: Receiving data in components

---
+++ src/components/basicos/ComParametro.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function ComParametros(props){
+    const status = props.nota >= 7 ? 'Aprovado(a)' : 'Retido(a)'
+    var nota = Math.ceil(props.nota);
+    return (
+        <div>
+            <h2>{props.titulo}</h2>
+            <p>
+                <strong>{props.aluno} </strong> 
+                tem nota
+                <strong> {nota} </strong>
+                e foi
+                <strong> {status}</strong>
+
+            </p>
+
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import './index.css'
 
 import ReactDOM from 'react-dom';
 import React from 'react';
-import Primeiro from './components/basicos/Primeiro.js';
 
+import Primeiro from './components/basicos/Primeiro';
+import ComParametro from './components/basicos/ComParametro';
 
 ReactDOM.render(
-    <div>
+    <div id='app'>
         <Primeiro></Primeiro>
+        <ComParametro titulo='Situação do Aluno' aluno='Pedro' nota={9.3}/>
+        <ComParametro titulo='Situação do Aluno' aluno='Maria' nota={9.9}/>
+
     </div>,
     document.getElementById('root')
 );
