438fb1435f01368164f80c5fd78ac5185428fdba
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-03-22 12:04:05 -0300

    Feat: Added list with repetition

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
 import RandomNum from './components/basicos/RandomNum';
 import Card from './components/layout/Card'
+import Familia from './components/basicos/Familia';
+import FamiliaMembro from './components/basicos/FamiliaMembro';
+import ListaAlunos from './components/repeticao/ListaAlunos'
+
 
 import './App.css'
 
@@ -12,19 +16,32 @@ export default _ =>
     <div className='App'>
         <h1>Fundamentos React</h1>
         <div className='Cards'>
-            <Card title='#4- Desafio Aleatório'>
+            
+            <Card title='#6- Repetição' color='#DD2C00'>
+                <ListaAlunos></ListaAlunos>
+            </Card>
+
+            <Card title='#5- Fragmento' color='#F9A825'>
+                <Familia sobrenome='Silva'>
+                    <FamiliaMembro nome='Pedro'/>
+                    <FamiliaMembro nome='Ana'/>
+                    <FamiliaMembro nome='Gustavo'/>
+                </Familia>
+            </Card>
+
+            <Card title='#4- Desafio Aleatório' color='#6200EA'>
                 <RandomNum min={3} max={10}/>
             </Card>
 
-            <Card title='#3- Fragmento'>
+            <Card title='#3- Fragmento' color='#5E35B1'>
                 <Fragmento />
             </Card>
 
-            <Card title='#2- Com Parametros'>
+            <Card title='#2- Com Parametros' color='#00BFA5'>
                 <ComParametro titulo='Situação do Aluno' aluno='Pedro' nota={9.3} />
             </Card>
 
-            <Card title='#1- Primeiro'>
+            <Card title='#1- Primeiro' color='#2E7D32'>
                 <Primeiro></Primeiro>
             </Card>
         </div>
---
+++ src/data/alunos.js
@@ -0,0 +1,11 @@
+export default [
+    {id: 1, nome: 'Ana', nota: 5.6},
+    {id: 2, nome: 'Bia', nota: 1.9},
+    {id: 3, nome: 'João', nota: 3.7},
+    {id: 4, nome: 'Mauricio', nota: 5.3},
+    {id: 5, nome: 'Lucas', nota: 6.5},
+    {id: 6, nome: 'Pedro', nota: 8.8},
+    {id: 7, nome: 'Jose', nota: 5.4},
+    {id: 8, nome: 'Carlos', nota: 2.6},
+    {id: 9, nome: 'Gustavo', nota: 10.0},
+]
